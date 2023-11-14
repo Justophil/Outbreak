0e06abb253c2f235d3b6b7744d03c24e78e2205b
damianovisa <damianovisa@gmail.com>
2023-11-14 18:24:36 -0500

    added healthbar, no functioning script rn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     public static float InitialScore; // Updated each Level, (may not be may be used)
     public static float Score = 0; // Overall Score, Synchronous with gameplay 
-    public static float Health; // Health of player, shared with player component for UI purpose
+    public static Slider Health; // Health of player, shared with player component for UI purpose
     public static float Stamina; // Stamina of player, shared with player component for UI purpose
     private void Awake()
     {
