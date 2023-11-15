fea85f317e44777a0c137e36c7f82c12cb43f0f4
damianovisa <damianovisa@gmail.com>
2023-11-14 22:02:16 -0500

    Added value to health bar

--- Assets/Scripts/PlayerHealthManager.cs
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthManager : MonoBehaviour
 {
+    public Slider healthBar;
+    float health = GameManager.Health;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthBar = GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthBar.value = health;
     }
+
+    
 }
